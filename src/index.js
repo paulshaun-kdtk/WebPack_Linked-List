81439617202b6157c421860bad51ed7db287ebc8
Shaun K <123934813+paulshaun-kdtk@users.noreply.github.com>
2023-07-20 12:06:55 +0200

    toDOList app structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
-import _ from 'lodash';
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
+const list = [
+  {
+    description: 'Make 9k by December 2023',
+    completed: false,
+    index: 3,
 
+  },
+  {
+    description: 'Buy a Kawasaki 2cc by November 2023',
+    completed: false,
+    index: 2,
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  },
+  {
+    description: 'Meet and greet with top 5 group',
+    completed: true,
+    index: 1,
 
-   return element;
- }
+  },
+];
 
- document.body.appendChild(component());
+const listContainer = document.getElementById('listContainer');
+
+// dynamic display
+function displayItemsByIndex() {
+  listContainer.innerHTML = '';
+
+  list.forEach((item) => {
+    const { description, completed, index } = item;
+
+    const listItem = document.createElement('div');
+    const descriptionSpan = document.createElement('span');
+    const completedRadio = document.createElement('input');
+    const hr = document.createElement('hr');
+
+    listItem.classList.add('icon');
+
+    descriptionSpan.textContent = description;
+    completedRadio.type = 'checkbox';
+    completedRadio.name = `completed_${index}`;
+    completedRadio.checked = completed;
+
+    listItem.appendChild(completedRadio);
+    listItem.appendChild(descriptionSpan);
+    listItem.appendChild(hr);
+
+    listContainer.appendChild(listItem);
+  });
+}
+
+displayItemsByIndex();
